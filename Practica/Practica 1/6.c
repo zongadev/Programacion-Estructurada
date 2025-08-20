1cbd9fb0d653e491e9e44d34799a3e4cd5f9b3cb
Goncho <67016020+zongadev@users.noreply.github.com>
2025-08-20 10:10:15 -0300

    mid6

--- Practica/Practica 1/6.c
+++ Practica/Practica 1/6.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 int T=100;
-void cargarText(char text[T]);
 
+int cargarText(char text[T]);
+void imprimirText(char text[T]);
+void normalizar(char text[T],int len);
 
-void cargarText(char text[T]){
-	printf("Ingrece un texto: \n");
+int cargarText(char text[T]){
+	printf("Ingrece un texto: ");
 	char c;
 	int i=0;
-	c=getchar();
-	while (c!='\n' && i<T){
+	while(i<T && ((c=getchar())!='\n')){
 		text[i]=c;
+		i++;
 	}
-	printf("%s",text);
+	text[i]='\0';
+	return i;
 }
 
-
+void imprimirText(char text[T]){
+	printf("\n Texto: \t %s",text);
+}
+	
+void normalizar(char text[T], int len){
+	char temp;
+	if(text[0]>=97 || text[0]<=122){
+		text[0]=text[0]-32;
+	}
+	for(int i=0; text[i]!= '\0';i++){ //el ciclo I es el que recorre la palabra
+		printf("i:[%c]",text[i]);
+		for(int j = i; text[j]==' '; j++){ //el ciclo J es el que itera dentro de los espacios
+			printf("j:[%c]",text[j]);
+			if(j>0){
+				
+				for(int k =i+1; text[i]!='\0';k++){//el ciclo K es para desplazar
+					printf("k:[%c]",text[k]);
+					text[k]= text[k+1];
+					k++;
+				}
+				
+		}
+	}
+}
+}
 int main(){
 	char text[T];
-	cargarText(text);
+	int l;
+	l=cargarText(text);
+	imprimirText(text);
+	normalizar(text,l);
+	imprimirText(text);
 }
