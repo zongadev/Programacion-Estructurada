940d24c5bb5bbc757c528126874b1e53b01763fa
Goncho <67016020+zongadev@users.noreply.github.com>
2025-09-11 15:37:14 -0300

    4y5

--- Practica/Practica 2/4/4.c
+++ Practica/Practica 2/4/4.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
 #include <string.h>
-
+#define N 5
 typedef struct persona{
 	char nombre[20];
 	char apellido[20];
 	int edad;
 }persona;
 
-void cargarArchivo(){
-	printf("Ingrese Nombre:\n");
-	char nombre[20],apellido[20],c;
-	int i =0;
-	while((c=getchar())!='\n' && i<20){
-		nombre[i]=c;
-		i++;
+void imprimir(char archivo[]){
+	FILE* arch = fopen(archivo,"r");
+	char nombre[20],apellido[20];
+	int edad;
+	printf("Nombre\t Apellido \tEdad\n");
+	int r = (int) fscanf(arch,"%19[^,],%19[^,],%d\n",nombre,apellido,&edad);
+	while(r!=EOF){
+		printf("%s\t %s \t%d\n",nombre,apellido,edad);
+		r = (int) fscanf(arch,"%19[^,],%19[^,],%d\n",nombre,apellido,&edad);
 	}
-	nombre[i]='\0';
-	printf("\nIngrese Apellido:\n");
-	while((c=getchar())!='\n' && i<20){
-		apellido[i]=c;
-		i++;
-	}
-	apellido[i]='\0';
-	printf("\nIngrese Edad:\n");
-	scanf("%d",edad);
+}
 	
-	fscanf()
+void cargarArchivo(char archivo[]){
+	FILE* arch =fopen(archivo, "w");
+	int i=0;
+	int j =1,edad,k=0;
+	char nombre[20],apellido[20],c;
+	while(i<N && j==1){
+		printf("-------------------------------------");
+		printf("\nIngrese Nombre:\n");
+		while((c=getchar())!='\n' && k<20){
+			nombre[k]=c;
+			k++;
+		}
+		
+		nombre[k]='\0';
+		k=0;
+		printf("\nIngrese Apellido:\n");
+		while((c=getchar())!='\n' && k<20){
+			apellido[k]=c;
+			k++;
+		}
+		apellido[k]='\0';
+		k=0;
+		printf("\nIngrese Edad:\n");
+		scanf("%d",&edad);
+		getchar();
+		if(edad!=-1){
+			fprintf(arch,"%s,%s,%d\n",nombre,apellido,edad);
+			i++;
+			
+		}else{
+			j=0;
+		}
+		
+	}
+	fclose(arch);
 }
 
 int main(){
-	cargarArchivo();
+	char archivo[] = "personas.txt";
+/*	cargarArchivo(archivo);*/
+	imprimir(archivo);
 	return 0;
 }
