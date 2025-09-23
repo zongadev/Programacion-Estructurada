7d9e49597c5afd86e05e6174cdd9c3cd5a5ad716
zonga <gonzimoyano@gmail.com>
2025-09-23 15:55:14 -0300

    sdada

--- Practica/Practica 3/5.c
+++ Practica/Practica 3/5.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct s_carta{
-	int numero,valor;
-	char palo;
-} t_carta;
+typedef struct tres{
+	int a,b,c;
+} t_tres;
 
-void ordenar(t_carta *p){
+void ordenar(t_tres *p){
+	int aux;
+	if(p->a > p->b){
+		aux = p->a;
+		p->a = p->b;
+		p->b = aux;
+		
+	}
+	if(p->a > p->c){
+		aux = p->a;
+		p->a = p->c;
+		p->c = aux;
+	}
 	
+	if(p->b > p->c){
+		aux = p->b;
+		p->b = p->c;
+		p->c = aux;
+	}
 }
+
 int main(){
-	t_carta cont[5];
-	cont[0].numero = 1;
-	cont[0].valor = ;
-	cont[0].palo = ;
-	
-	cont[1].numero = ;
-	cont[1].valor = ;
-	cont[1].palo = ;
-	
-	cont[2].numero = ;
-	cont[2].valor = ;
-	cont[2].palo = ;
+	t_tres t;
+	t.a = 3;
+	t.c = 1;
+	t.b = 8;
+	ordenar(&t);
+	printf("%d,%d,%d",t.a,t.b,t.c);
 	return 0;
 }
